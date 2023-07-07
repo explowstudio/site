d3d5e682e83fc1cf17b5987ea73634786e19fa3f
Gabriel <oi@gabrielteodoro.com>
2023-07-07 20:44:18 -0300

    feat(pages): add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import type { AppProps } from "next/app";
 
+import { globalStyles } from "@/ui/stitches.config";
+
 export default function App({ Component, pageProps }: AppProps) {
+  globalStyles();
+
   return <Component {...pageProps} />;
 }
