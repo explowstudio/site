5bc6b13e92507f4b54964e3f9cc1aa088f4f0f23
Gabriel <oi@gabrielteodoro.com>
2023-07-07 22:50:30 -0300

    feat(pages): add fonts in document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,14 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Anek+Tamil:wght@400;500;600;700&family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+
         <style
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
