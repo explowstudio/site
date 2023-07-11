8779d1feef6ae14135b3ad76dbf89d2b1aa02f8b
Gabriel <oi@gabrielteodoro.com>
2023-07-11 15:18:54 -0300

    feat(ui): add link component

---
+++ src/ui/Link/Link.ts
@@ -0,0 +1,63 @@
+import NextLink from "next/link";
+
+import { styled } from "@/ui/stitches.config";
+
+export const Link = styled(NextLink, {
+  position: "relative",
+
+  display: "flex",
+  alignItems: "center",
+  gap: "$2",
+
+  fontSize: "$sm",
+  lineHeight: "$sm",
+  fontWeight: "$medium",
+
+  transition: "$default",
+  width: "fit-content",
+
+  variants: {
+    linkUnderline: {
+      true: {
+        "&:hover::after": {
+          transform: "scale(1)",
+          transformOrigin: "left",
+        },
+
+        "&::after": {
+          content: "",
+          position: "absolute",
+          bottom: "-4px",
+          left: 0,
+          width: "100%",
+          height: "1px",
+          transform: "scaleX(0)",
+          transformOrigin: "right",
+          transition: "transform .4s $easeOutCubic",
+          willChange: "transform",
+        },
+      },
+    },
+    color: {
+      primary: {
+        color: "$black",
+
+        "&::after": {
+          background: "$black",
+        },
+      },
+      secondary: {
+        color: "$gray500",
+
+        "&::after": {
+          background: "$gray500",
+        },
+      },
+    },
+  },
+
+  defaultVariants: {
+    linkUnderline: true,
+    color: "primary",
+  },
+});
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,4 +2,5 @@ export * from "./Button";
 export * from "./Divider";
 export * from "./Grid";
 export * from "./IconButton";
+export * from "./Link";
 export * from "./Logo";
