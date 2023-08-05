d85c5b29b1979ff64987e986e183e45d32b6f61e
Gabriel <oi@gabrielteodoro.com>
2023-08-05 12:00:09 -0300

    feat(transitions): add fade-in transition

---
+++ src/ui/_transitions/FadeIn.tsx
@@ -0,0 +1,58 @@
+import { Target, TargetAndTransition, motion } from "framer-motion";
+import { ComponentElement, ElementType, ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  duration?: number;
+  delay?: number;
+  initial?: Target;
+  animate?: TargetAndTransition;
+  component?: ElementType;
+};
+
+export function FadeIn({
+  children,
+  duration = 0.8,
+  delay = 0,
+  initial = {},
+  animate = {},
+  component = "div",
+  ...rest
+}: Props) {
+  const MotionComponent = motion(component);
+
+  return (
+    <MotionComponent
+      initial={{ opacity: 0, ...initial }}
+      animate={{ opacity: 1, ...animate }}
+      transition={{ duration, delay }}
+      {...rest}
+    >
+      {children}
+    </MotionComponent>
+  );
+}
+
+FadeIn.WhileInView = function WhileInView({
+  children,
+  duration = 0.8,
+  delay = 0,
+  initial = {},
+  animate = {},
+  component = "div",
+  ...rest
+}: Props) {
+  const MotionComponent = motion(component);
+
+  return (
+    <MotionComponent
+      initial={{ opacity: 0, ...initial }}
+      whileInView={{ opacity: 1, ...animate }}
+      transition={{ duration, delay }}
+      viewport={{ once: true }}
+      {...rest}
+    >
+      {children}
+    </MotionComponent>
+  );
+};
