d41561a97a8168d104de8010c55168f7caf0f2ac
Gabriel <oi@gabrielteodoro.com>
2023-08-08 23:10:40 -0300

    feat: add google analytics

---
+++ src/lib/GoogleAnalytics.tsx
@@ -0,0 +1,23 @@
+import Script from "next/script";
+
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
+
+export function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', "${GA_TRACKING_ID}");
+        `}
+      </Script>
+    </>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from "next/app";
 
+import { GoogleAnalytics } from "@/lib";
 import { globalStyles } from "@/ui/stitches.config";
 
 export default function App({ Component, pageProps }: AppProps) {
   globalStyles();
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics />
+      <Component {...pageProps} />
+    </>
+  );
 }
