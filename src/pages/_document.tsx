a7e73ad8e61ee6f5bd2041247ad4937715a3834b
oigabrielteodoro <gabriel.teodoro@tino.com.br>
2024-02-03 18:02:43 -0300

    chore: add clarityms

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,17 @@ export default function Document() {
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
         />
+
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "kuzd8e6i4p");`,
+          }}
+        />
       </Head>
       <body>
         <Main />
